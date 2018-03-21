3742d4eb685100ea26784de9a62ea20cbbcf15e4
Your Name <zhuchanyoon@gmail.com>
2018-03-21 17:31:10 -0500

    adds output dir check

--- redactor/redactor.py
+++ redactor/redactor.py
@@ -1,45 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import argparse
+import nltk
 
 def main(files, flags, concepts, output_dir, stats):
 
     # handle input files
-    for f in files:
+    for file in files:
         # check if file exists
-        print(f)
-    
-    # redact items specified in flags
-    if "names" in flags:
-        # redact names
-        print("redact names")
-    if "genders" in flags:
-        # redact genders
-        print("redact genders")
-    if "dates" in flags:
-        # redact dates
-        print("redact dates")
-    if "addresses" in flags:
-        # redact addresses
-        print("redact addresses")
-    if "phones" in flags:
-        # redact phones
-        print("redact phones")
-    
-    # redact themes specified in concepts
-    if len(concepts) > 0:
-        for concept in concepts:
-            # redact concept
-            print(concept)
+        with open(file, 'r') as f:
+            raw_content = f.read()
 
-    # handle output
-    # ***TODO*** output not dir
-    print(output_dir)
+            # redact items specified in flags
+        if "names" in flags:
+            # redact names
+            print("redact names")
+        if "genders" in flags:
+            # redact genders
+            print("redact genders")
+        if "dates" in flags:
+            # redact dates
+            print("redact dates")
+        if "addresses" in flags:
+            # redact addresses
+            print("redact addresses")
+        if "phones" in flags:
+            # redact phones
+            print("redact phones")
+        
+        # redact themes specified in concepts
+        if len(concepts) > 0:
+            for concept in concepts:
+                # redact concept
+                print(concept)
 
-    # handle stats
-    print(stats)
+        # handle output
+        # ***TODO*** output not dir
+        print(output_dir)
+
+        # handle stats
+        print(stats)
     
+
+### parse args ###
+
+class FullPaths(argparse.Action):
+    """Expand user- and relative-paths"""
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, os.path.abspath(os.path.expanduser(values)))
+
+def is_dir(dirname):
+    """Checks if a path is an actual directory"""
+    if not os.path.isdir(dirname):
+        msg = "{0} is not a directory".format(dirname)
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return dirname   
      
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -52,7 +70,7 @@ if __name__ == '__main__':
     parser.add_argument("--phones", action='store_true', help="redact phones")
     parser.add_argument("--concept", action='append',
                         help="The theme/keyword to redact")
-    parser.add_argument("--output", required=True, help="directory to store redacted files")
+    parser.add_argument("--output", required=True, help="directory to store redacted files", action=FullPaths, type=is_dir)
     parser.add_argument("--stats", type=argparse.FileType('w'), required=True,
                         help="file storing the summary of redaction process")
     args = parser.parse_args()
